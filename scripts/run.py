d48cbba8ea22987afec0a70de0e3f51100e4b357
aman <aman.nalakath@tum.de>
2025-12-03 09:43:21 +0000

    reorganize and tidying

--- scripts/run.py
+++ scripts/run.py
@@ -51,8 +51,8 @@ def main():
 
     #2 no matrices i.e to fetch from databases
     else:
-        # run(["python", "scripts/api_hca_userinp.py"])
-        run(["bash", "scripts/run_in_scanpy.sh", "scripts/api_hca_userinp.py"])
+        # run(["python", "src/api_hca_userinp.py"])
+        run(["bash", "src/run_in_scanpy.sh", "src/api_hca_userinp.py"])
         # adjust this to the fetch script outputs
         with open("data_raw/_last_downloaded.txt") as f: # using _last_downloaded.txt as identifier
             input_file = f.read().strip()
@@ -62,31 +62,31 @@ def main():
     # branch A
     if args.branch == "annotate_cells":
         # normalize per cell, apply log transform to pass onto celltypist. Clustering could be added, but CellTypist can do clustering on its own
-        run(["bash", "scripts/run_in_scanpy.sh", "scripts/scanpy_norm.py", "--input", input_file])
-        run(["bash", "scripts/run_in_scanpy.sh", "scripts/annotate_celltypes.py"])
+        run(["bash", "src/run_in_scanpy.sh", "src/scanpy_norm.py", "--input", input_file])
+        run(["bash", "src/run_in_scanpy.sh", "src/annotate_celltypes.py"])
         return
 
     # branch B
     if args.branch == "metabolic":
         # 1. norm_pooling.py = Normalize by pooling all celltypes or extract cell types # psuedo-bulk
-        cmd = ["bash", "scripts/run_in_scanpy.sh", "scripts/norm_pooling.py"]
+        cmd = ["bash", "src/run_in_scanpy.sh", "src/norm_pooling.py"]
         if input_file: cmd += ["--input", input_file]
         if celltype is not None:
             cmd += ["--celltype", celltype]
         run(cmd)
 
         # 2. genetoensembl.py = To change the HGNC symbol to Ensembl ids (compatible with HumanGEM). #TODO : EntrezId for Recon3D
-        cmd = ["bash", "scripts/run_in_scanpy.sh", "scripts/genetoensembl.py"] # I need to run this before context algorithms
+        cmd = ["bash", "src/run_in_scanpy.sh", "src/genetoensembl.py"] # I need to run this before context algorithms
         if gtf: cmd += ["--gtf", gtf]
         run(cmd) 
 
         # 3. Any of the algorithm. GIMME initially #TODO: specifiable in config
-        cmd = ["bash", "scripts/run_in_cplex.sh", "scripts/gimme.py"]
+        cmd = ["bash", "src/run_in_cplex.sh", "src/gimme.py"]
         model_path = cfg.get("model")
         if model_path:
             cmd += ["--model", model_path]
         run(cmd)
-        #run(["bash", "scripts/run_in_cplex.sh", "scripts/tinit.py"])
+        #run(["bash", "src/run_in_cplex.sh", "src/tinit.py"])
         return
 
 if __name__ == "__main__":
