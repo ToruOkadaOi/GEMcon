af97c19f0cc7ad763d2aa01d67dcc269dc27e536
am_nala <aman.nalakath@tum.de>
2026-04-01 16:09:13 +0000

    cleanup: remove notebooks dir, move gtex_explanations to docs

--- notebooks/hpo/ontology-service-hp-client/ontology_service_hp_client/__init__.py
+++
@@ -1,8 +0,0 @@
-"""A client library for accessing ontology-service-hp"""
-
-from .client import AuthenticatedClient, Client
-
-__all__ = (
-    "AuthenticatedClient",
-    "Client",
-)
--- notebooks/hpo/ontology-service-hp-client/ontology_service_hp_client/models/translation_status.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class TranslationStatus(str, Enum):
-    CANDIDATE = "CANDIDATE"
-    OFFICIAL = "OFFICIAL"
-
-    def __str__(self) -> str:
-        return str(self.value)
